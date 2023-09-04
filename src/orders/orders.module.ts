1bad8890923002d63ba90f4c4d52e06e8001d8dd
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-04 20:53:21 -0300

    feat: config ClientOrders Kafka message broker

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
-import { ClientsModule } from '@nestjs/microservices';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports:[ClientsModule.register]
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ORDERS_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'orders',
+            brokers: ['localhost:9092'],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
