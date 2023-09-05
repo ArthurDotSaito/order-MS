8bb205029145185edb05680df7c70e59abddcffa
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-04 23:07:35 -0300

    feat: implementing promise from observable on create method in orders.service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateOrderDTO } from './create-orders.dto';
 import { OrderStatus } from '@prisma/client';
+import { ClientKafka } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    @Inject('ORDERS_SERVICE')
+    private readonly kafkaClient: ClientKafka,
+  ) {}
 
   all() {
     return this.prismaService.order.findMany();
@@ -18,6 +24,8 @@ export class OrdersService {
         status: OrderStatus.PENDING, // Enum gerado pelo prisma
       },
     });
+
+    await lastValueFrom(this.kafkaClient.emit('orders', order);)
     return order;
   }
 }
