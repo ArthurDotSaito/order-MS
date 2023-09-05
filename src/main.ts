463b62e6bd3c153f3d62c054ff19a39785a928b2
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-04 23:55:37 -0300

    feat: enable message consume on main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'orders-consumer',
+      },
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
